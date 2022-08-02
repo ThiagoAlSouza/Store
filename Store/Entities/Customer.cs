5783583e3d39fff86fece6dd4940be23b38e1b1c
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-02 08:49:19 -0300

    Add project files.

---
+++ Store/Entities/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace Store.Entities;
+
+public class Customer
+{
+    #region Contructors
+
+    public Customer(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+
+    #endregion
+}
