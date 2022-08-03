b040370f91c2f6de2a80b5cdd99bc3c0ed3107ea
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-03 09:04:11 -0300

    Implementação primeiro teste entidade Order

---
+++ Store/Tests/OrderTests.cs
@@ -0,0 +1,16 @@
+﻿using Store.Entities;
+
+namespace Store.Tests;
+
+public class OrderTests
+{
+    [Fact]
+    public void WhenCreatedAOrderShouldReturnNumberWith8Characters()
+    {
+        var customer = new Customer("Thiago", "thiagoalsouza98@outlook.com.br");
+        var discount = new Discount(50, new DateTime(2022, 08, 03));
+        var order = new Order(customer, discount, 10);
+
+        Assert.Equal(8, order.Number.Length);
+    }
+}
