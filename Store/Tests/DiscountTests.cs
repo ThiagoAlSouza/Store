717a511ea80eca61356c0490c2896d6fc2352ded
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-02 22:27:52 -0300

    Implementação testes da classe Discount

---
+++ Store/Tests/DiscountTests.cs
@@ -0,0 +1,23 @@
+﻿using Store.Entities;
+
+namespace Store.Tests;
+
+public class DiscountTests
+{
+    [Theory]
+    [InlineData("2022, 08, 02")]
+    public void ShouldReturnErrorWhenDiscountIsNotValid(DateTime date) 
+    {
+        var discount = new Discount(60, new DateTime(2022, 08, 02));
+
+        Assert.True(!discount.IsValid());
+    }
+
+    [Fact]
+    public void ShouldReturnSucessWhenDiscountIsValid()
+    {
+        var discount = new Discount(60, new DateTime(2022, 08, 25));
+
+        Assert.True(discount.IsValid());
+    }
+}
