a21296bf4dfa2cea1b41476570da61cf3cc63d4f
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-03 13:48:39 -0300

    Implementação classe estática ProdutoQueries

---
+++ Store/Queries/ProductQueries.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using Store.Entities;
+
+namespace Store.Queries;
+
+public static class ProductQueries
+{
+    public static Expression<Func<Product, bool>> GetActivateProducts()
+    {
+        return x => x.Active;
+    }
+
+    public static Expression<Func<Product, bool>> GetInativeProducts()
+    {
+        return x => !x.Active;
+    }
+}
